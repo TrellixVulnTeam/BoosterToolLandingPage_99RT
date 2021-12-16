570e4b156e0ba70e7227665058fb2dc64aaa91bc
YeyoM <yeyomoreno2003@hotmail.com>
2021-12-15 20:29:06 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const app               = require('./server');
+                          //require('./db');
+
+const port = app.get('port');
+const host = app.get('host');
+
+app.listen(port, host, () => {
+    console.log('server running');
+});
