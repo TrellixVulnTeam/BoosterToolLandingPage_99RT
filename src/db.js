5f4c6d244542d7fb5954434e234c7dcedbbe382f
YeyoM <yeyomoreno2003@hotmail.com>
2021-12-27 12:16:04 -0600

    debugging

--- src/db.js
+++ src/db.js
@@ -5,12 +5,12 @@ const mongoose          = require('mongoose');
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
 
 
-// cambiar a prod para produccion y quitar los // para que funcione al 100
+// cambiar a prod para produccion 
 mongoose.connect(MONGODB_URI_PROD, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
 })
     .then(db => console.log('DB conected succesfully'))
     .catch(err => console.log(err));
