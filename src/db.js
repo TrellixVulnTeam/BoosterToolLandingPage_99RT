a1ad5a8a03e6b5759befe626ef2759d54cf3a411
YeyoM <yeyomoreno2003@hotmail.com>
2021-12-27 11:52:43 -0600

    PROD

--- src/db.js
+++ src/db.js
@@ -1,16 +1,16 @@
 const mongoose          = require('mongoose');
 
-const { AUTH_APP_MONGODB_HOST, AUTH_APP_MONGODB_DATABASE} = process.env;
-const MONGODB_URIS_LOCAL = `mongodb://${AUTH_APP_MONGODB_HOST}/${ AUTH_APP_MONGODB_DATABASE}`;
+//const { AUTH_APP_MONGODB_HOST, AUTH_APP_MONGODB_DATABASE} = process.env;
+//const MONGODB_URI_LOCAL = `mongodb://${AUTH_APP_MONGODB_HOST}/${ AUTH_APP_MONGODB_DATABASE}`;
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
 
 
 // cambiar a prod para produccion y quitar los // para que funcione al 100
-mongoose.connect(MONGODB_URIS_LOCAL, {
+mongoose.connect(MONGODB_URI_PROD, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    //useCreateIndex: true,
-    //useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false,
 })
     .then(db => console.log('DB conected succesfully'))
     .catch(err => console.log(err));
